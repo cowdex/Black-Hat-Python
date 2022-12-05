aec4d31ffce9c97adc97c12ee91d8d858459c6ab
cowdex <dhikmohamed05@gmail.com>
2022-12-05 22:12:16 +0100

    first step

---
+++ Basic Networking Tools/netcat.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import argparse
+import socket
+import threading
+import sys
+import textwrap
+import subprocess
+import shlex 
+
+#The shlex module implements a class for parsing simple shell-like syntaxes.
+#subprocess process-creation inter-face
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDERR)
+
+    return output.decode()
+
